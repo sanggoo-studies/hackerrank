271a6cd7063f5a91ea74bfab0517a196b4cef2b5
Sanggoo Seo <sanggoo@sanggoo.com>
2018-05-01 23:41:25 +0900

    Practice > Algorithms > Implementation > Beautiful Days at the Movies
    https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem

---
+++ practice/algorithms/implementation/Beautiful Days at the Movies.py
@@ -0,0 +1,23 @@
+import sys
+from io import StringIO
+
+
+def beautifulDays(i, j, k):
+    count = 0
+    for n in range(i, j+1):
+        if abs(n - int(str(n)[::-1])) % k == 0:
+            count += 1
+
+    return count
+
+
+sys.stdin = StringIO("""20 23 6
+""")
+while 1:
+    try:
+        i, j, k = input().strip().split(' ')
+        i, j, k = [int(i), int(j), int(k)]
+        result = beautifulDays(i, j, k)
+        print(result)
+    except EOFError:
+        break
